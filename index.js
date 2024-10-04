933039280e2dd1a073292b55ac13745ec15cb1f5
Mônica Colomera <colomeramonica@gmail.com>
2024-10-04 08:09:43 -0300

    english

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const mongoose = require('mongoose');
+const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
@@ -9,11 +9,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB', error);
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Servidor em execução na porta ${PORT}`);
+  console.log(`🚀 Server running on ${PORT}`);
 });
--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,6 @@ const User = require('../models/User');
 
 const router = express.Router();
 
-// Registro de usuário  
 router.post('/signup', async (req, res) => {
   const { username, email, password } = req.body;
 
@@ -13,20 +12,19 @@ router.post('/signup', async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({ username, email, password: hashedPassword });
     await user.save();
-    res.status(201).json({ message: 'Usuário criado com sucesso!' });
+    res.status(201).json({ message: 'User created succesfully!' });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
 
-// Login de usuário  
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   try {
     const user = await User.findOne({ email });
     if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(401).json({ message: 'Email ou senha incorretos.' });
+      return res.status(401).json({ message: 'Email or password incorrect.' });
     }
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
